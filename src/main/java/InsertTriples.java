bf0586ae0bb29194dd0d463dc81836c02dc05fc4
m0hitbansal <mohit.bansal@iiitb.org>
2020-04-01 22:35:25 +0530

    insert triples in database

--- src/main/java/InsertTriples.java
+++ src/main/java/InsertTriples.java
@@ -3,12 +3,12 @@ import org.apache.jena.query.ResultSet;
 import Ontology2SDB2MySQL.*;
 public class InsertTriples {
 	public static void main(String args[]) throws Exception {
-		String filename = "/home/mohit/git/SWRE---A-rule-engine-for-semantic-web/Movie-Ontology.owl";
+		String filename = "/home/mohit/git/SWRE---A-rule-engine-for-semantic-web/University.owl";
 		String namespace = "SWRE";
-		String prefix = "http://www.iiitb.ac.in/MovieOntology#";
+		String prefix = "http://www.iiitb.org/university#";
 		String subject="mohit";
-		String predicate="Acted_In";
-		String object="Don";
+		String predicate="studies";
+		String object="PS202";
 		SDBUtilities.JDBCinit();
 		String rs =SDBUtilities.Inserttriples(filename, namespace, prefix,subject,predicate,object);
 		
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,14 +4,13 @@ import Ontology2SDB2MySQL.*;
 
 public class Test {
 	public static void main(String args[]) throws Exception {
-		String filename = "/home/mohit/git/SWRE---A-rule-engine-for-semantic-web/Movie-Ontology.owl";
+		String filename = "/home/mohit/git/SWRE---A-rule-engine-for-semantic-web/University.owl";
 		String namespace = "SWRE";
-		String prefix = "http://www.iiitb.ac.in/MovieOntology#";
+		String prefix = "http://www.iiitb.org/university#";
 
 		SDBUtilities.JDBCinit();
-//		SDBUtilities.ont2SDB2SQL(filename, namespace, prefix);
-		String query = "PREFIX foo:<http://www.iiitb.ac.in/MovieOntology#>" + 
-		"SELECT distinct ?Actors  {?Actors foo:Acted_In foo:Don}";
+		//SDBUtilities.ont2SDB2SQL(filename, namespace, prefix);
+		String query =  "PREFIX foo:<http://www.iiitb.org/university#>" +"SELECT *  {?s foo:studies foo:PS202}";
 		ResultSet rs = SDBUtilities.SDBQuery(query);
 		System.out.print(rs);
 		System.out.println("Done");

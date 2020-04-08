c9cf1818718bd583c6279a22997ecfbe6ba8731f
Parth <parthendo@geu.ac.in>
2020-04-08 17:45:25 +0530

    Rule Store added

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,20 +1,48 @@
+import java.util.ArrayList;
+
 import org.apache.jena.query.ResultSet;
 
 import Ontology2SDB2MySQL.*;
+import ruleGenerator.RuleBox;
 
 public class Test {
 	public static void main(String args[]) throws Exception {
-		String filename = "University.owl";
-		String namespace = "SWRE";
-		String prefix = "http://www.iiitb.org/university#";
-
-		SDBUtilities.JDBCinit();
-		// Create Database
-		// SDBUtilities.ont2SDB2SQL(filename, namespace, prefix);
-		String query =  "PREFIX foo:<http://www.iiitb.org/university#>" +
-						"SELECT *  {?s foo:studies foo:PS202}";
-		ResultSet rs = SDBUtilities.SDBQuery(query);
-		System.out.print(rs);
+//		String filename = "University.owl";
+//		String namespace = "SWRE";
+//		String prefix = "http://www.iiitb.org/university#";
+//
+//		SDBUtilities.JDBCinit();
+//		// Create Database
+//		// SDBUtilities.ont2SDB2SQL(filename, namespace, prefix);
+//		String query =  "PREFIX foo:<http://www.iiitb.org/university#>" +
+//						"SELECT *  {?s foo:studies foo:PS202}";
+//		ResultSet rs = SDBUtilities.SDBQuery(query);
+//		System.out.print(rs);
+		String[] ant = new String[7];
+		  ant[0]="teacher";
+		  ant[1]="teaches";
+		  ant[2]="course";
+		  ant[3]="AND";
+		  ant[4]="student";
+		  ant[5]="enrolledIn";
+		  ant[6]="course";
+		  String[] con = new String[3];
+		  con[0]="student";
+	 	  con[1]="isStudentOf";
+		  con[2]="teacher";
+		RuleBox obj = new RuleBox();
+		obj.init();
+		obj.addRule(ant,con);
+		ArrayList<ArrayList<String>> arr = obj.getRules();
+		int l = arr.size();
+		System.out.println();
+		for(int i=0;i<l;i++) {
+			ArrayList<String> temp = arr.get(i);
+			int l1 = temp.size();
+			for(int j=0;j<l1;j++)
+				System.out.print(temp.get(j)+" ");
+			System.out.println();
+		}
 		System.out.println("Done");
 	}
 }
---
+++ src/main/java/ruleGenerator/RuleBox.java
@@ -0,0 +1,224 @@
+package ruleGenerator;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Properties;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+
+/*
+ * 
+ * INSTEAD OF SIMPLE RULES AND RULE NODE IN XML, MENTION A PREFIX IN CONFIG
+ * AND APPEND THAT HERE IN FRONT OF ALL THE NODE NAMES
+ */
+
+public class RuleBox {
+
+	private static String xmlFilename;
+	private static File xmlFile;
+	
+	public RuleBox() {
+		xmlFilename = null;
+		xmlFile = null;
+	}
+	
+	public void init() throws Exception {
+		
+		// Reading rules file of XML from dbconfig
+		FileInputStream fis = null;
+		try {
+		 fis = new FileInputStream("dbconfig.prop");
+		} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		}
+		Properties property = new Properties();
+		property.load(fis);
+		xmlFilename = (String)property.get("RULE_STORE");
+		
+		try {
+			xmlFile = new File(xmlFilename);
+			if(xmlFile.createNewFile()) {
+				FileWriter initXML = new FileWriter(xmlFilename);
+				initXML.write("<Rules>\n</Rules>");
+				initXML.close();
+			}
+			else {
+				System.out.print("File Exists");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void addRule(String Antecedent[], String Consequent[]) throws SAXException, IOException, TransformerConfigurationException {
+		
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = null;
+		try {
+			documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Document document = documentBuilder.parse(xmlFilename);
+		//Fetches the root element
+	    Element rootElement = document.getDocumentElement();
+	    
+	    /*
+	     * Type 0: A subject
+	     * Type 1: A predicate
+	     * Type 2: An object
+	     * Type 3: A connector
+	     */
+	    int type = 0;
+	    // Consequent length always 3
+	    int antecedentLength = Antecedent.length;
+	    
+	    Element rule = document.createElement("Rule");
+		rootElement.appendChild(rule);
+		Element antecedent = document.createElement("Antecendent");
+		Element consequent = document.createElement("Consequent");
+	    
+	    for(int i = 0; i < antecedentLength; i++) {
+	    	
+	    	switch(type) {
+	    		case 0:
+	    			Element subject = document.createElement("Subject");
+	    			subject.appendChild(document.createTextNode(Antecedent[i]));
+	    			antecedent.appendChild(subject);
+	    			type = 1;
+	    			break;
+	    		case 1:
+	    			Element predicate = document.createElement("Predicate");
+	    			predicate.appendChild(document.createTextNode(Antecedent[i]));
+	    			antecedent.appendChild(predicate);
+	    			type = 2;
+	    			break;
+	    		case 2:
+	    			Element object = document.createElement("Object");
+	    			object.appendChild(document.createTextNode(Antecedent[i]));
+	    			antecedent.appendChild(object);
+	    			type = 3;
+	    			break;
+	    		case 3:
+	    			Element connector = document.createElement("Connector");
+	    			connector.appendChild(document.createTextNode(Antecedent[i]));
+	    			antecedent.appendChild(connector);
+	    			type = 0;
+	    			break;
+	    	}
+	    }
+	    
+	    Element subject = document.createElement("Subject");
+		subject.appendChild(document.createTextNode(Consequent[0]));
+		consequent.appendChild(subject);
+		Element predicate = document.createElement("Predicate");
+		predicate.appendChild(document.createTextNode(Consequent[1]));
+		consequent.appendChild(predicate);
+		Element object = document.createElement("Object");
+		object.appendChild(document.createTextNode(Consequent[2]));
+		consequent.appendChild(object);
+	    
+	    rule.appendChild(antecedent);
+	    rule.appendChild(consequent);
+	    
+	    DOMSource source = new DOMSource(document);
+	    TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	    Transformer transformer = transformerFactory.newTransformer();
+	    StreamResult result = new StreamResult(xmlFilename);
+	    try {
+			transformer.transform(source, result);
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<ArrayList<String>> getRules() throws SAXException, IOException{
+		
+		ArrayList<ArrayList<String>> ruleList = new ArrayList<ArrayList<String>>();
+		
+		xmlFile = new File(xmlFilename);
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = null;
+		try {
+			dBuilder = dbFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Document doc = dBuilder.parse(xmlFile);
+        doc.getDocumentElement().normalize();
+        NodeList nList = doc.getElementsByTagName("Rule");
+        int len = nList.getLength();
+        
+        for( int itr = 0; itr < len; itr++) {
+        	
+        	ArrayList<String> rule = new ArrayList<>();
+        	
+        	Node nNode = nList.item(itr);
+        	Element nElement = (Element) nNode;
+        	
+        	Node antecedent = nElement.getElementsByTagName("Antecendent").item(0);
+    	    Node consequent = nElement.getElementsByTagName("Consequent").item(0);
+    	    
+    	    Element eElement = (Element) antecedent; 
+    	    Element cElement = (Element) consequent;
+    	    
+    	    NodeList subject = eElement.getElementsByTagName("Subject");
+    	    NodeList predicate = eElement.getElementsByTagName("Predicate");
+    	    NodeList object = eElement.getElementsByTagName("Object");
+    	    NodeList connector = eElement.getElementsByTagName("Connector");
+    	    
+    	    int subjectLength = subject.getLength();
+    	    int predicateLength = predicate.getLength();
+    	    int objectLength = object.getLength();
+    	    int connectorLength = connector.getLength();
+    	    int tempLength = subjectLength;
+    	    
+    	    int loop = 0;
+    	    while(loop<tempLength) {
+    	    	
+    	    	if(nNode.getNodeType() == Node.ELEMENT_NODE) {
+    	    		
+    	    		rule.add(eElement.getElementsByTagName("Subject").item(loop).getTextContent());
+    	    		
+    	    		if(loop < predicateLength)
+    	    			rule.add(eElement.getElementsByTagName("Predicate").item(loop).getTextContent());
+    	    		if(loop< objectLength)
+    	    			rule.add(eElement.getElementsByTagName("Object").item(loop).getTextContent());
+    	    		if(loop<connectorLength)
+    	    			rule.add(eElement.getElementsByTagName("Connector").item(loop).getTextContent());
+    	    		loop++;
+    	    	}
+    	    }
+    	    rule.add(cElement.getElementsByTagName("Subject").item(0).getTextContent());
+    	    rule.add(cElement.getElementsByTagName("Predicate").item(0).getTextContent());
+    	    rule.add(cElement.getElementsByTagName("Object").item(0).getTextContent());
+    	    ruleList.add(rule);
+        }
+		return ruleList;
+	}
+}
